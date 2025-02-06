f2d52392529e788a8e6ba4f00e4cc9b437fd21bc
j8n17 <j3837301@gmail.com>
2025-02-06 17:39:17 +0900

    Initial Commit

---
+++ MyPortfolio/MyPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPortfolioApp.swift
+//  MyPortfolio
+//
+//  Created by 김준 on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MyPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
